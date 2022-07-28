b49fcd95c472a19cb46bdd80dec67cf2355fa285
Anurag Tyagi <anuragtyagi4964@gmail.com>
2022-07-29 00:17:13 +0530

    Added default values for timestamp and ishiring field

--- 06-create-conversation-table.sql
+++ 06-create-conversation-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE conversation(
     user_name VARCHAR(200),
     employer_name VARCHAR(300),
     message TEXT,
-    date_sent TIMESTAMP
+    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
