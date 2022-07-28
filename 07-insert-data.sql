6d0001c1e1c16b655f91aa9c47c87d872eec184a
Anurag Tyagi <anuragtyagi4964@gmail.com>
2022-07-29 00:16:50 +0530

    Inserting data into employers and conversation table

---
+++ 07-insert-data.sql
@@ -0,0 +1,47 @@
+-- INSERT INTO employers (
+--         company_name,
+--         company_address,
+--         yearly_revenue,
+--         is_hiring
+--     )
+-- VALUES ('Learning Inc', 'Seattle, USA', 0.87, FALSE);
+-- INSERT INTO employers (
+--         company_name,
+--         company_address,
+--         yearly_revenue,
+--         is_hiring
+--     )
+-- VALUES (
+--         'Wayne Enterprises',
+--         'Gotham City, USA',
+--         3.7,
+--         TRUE
+--     );
+-- INSERT INTO employers (
+--         company_name,
+--         company_address,
+--         yearly_revenue,
+--         is_hiring
+--     )
+-- VALUES ('Stark Industries', 'New York, USA', 8.35, TRUE);
+INSERT INTO conversation (user_name, employer_name, message, date_sent)
+VALUES (
+        'Anurag Tyagi',
+        'Stark Industries',
+        'This is the text',
+        '2021-10-15 16:32:10'
+    );
+INSERT INTO conversation (user_name, employer_name, message, date_sent)
+VALUES (
+        'Archana Sharma',
+        'Stark Industries',
+        'This is the text',
+        '2021-10-15 16:32:10'
+    );
+INSERT INTO conversation (user_name, employer_name, message, date_sent)
+VALUES (
+        'Dee Harshita',
+        'Wayne Enterprises',
+        'This is the text',
+        '2021-10-15 16:32:10'
+    );
